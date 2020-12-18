412d80667ee9ffd9da478700288ab5e5e402019a
Leon RO <leon.rodriguez.ore@gmail.com>
2020-12-17 23:04:05 -0300

    Aplicación del método Binding , reemplazando el findview ,aplicación de constraint , fondo de app
    Signed-off-by: Leon RO <leon.rodriguez.ore@gmail.com>

--- app/src/main/java/com/example/registrodeusuario/MainActivity.java
+++ app/src/main/java/com/example/registrodeusuario/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.registrodeusuario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.registrodeusuario.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding mbinding1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mbinding1 =ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(mbinding1.getRoot());
+
+        //edición utilizando método Binding
+        mbinding1.button.setText("Registrar");
+        mbinding1.editTextTextEmailAddress.setText("correo@gmail.com");
+        mbinding1.editTextTextPersonName2.setText("Jesús");
+        mbinding1.editTextTextPersonName3.setText("Rodríguez");
+        mbinding1.button.setText("Registrar");
+
+
+
     }
 }
